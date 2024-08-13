850e3428aa282a61d40fb2a45101dd258a976fe3
TAINA VIEIRA DE ARAUJO <00001107574183sp@aluno.educacao.sp.gov.br>
2024-08-13 13:29:10 -0300

    inserir dados

--- dbcontrol.js
+++ dbcontrol.js
@@ -10,7 +10,10 @@ async function criarTabela(login, senha){
     //Criação da criarTabela
     db.exec('CREATE TABLE IF NOT EXISTS login (id INTEGER PRIMARY KEY AUTOINCREMENT, login VARCHAR, senha VARCHAR)');
 
+    //Inserção dos dados
+    db.run('INSERT INTO login (login, senha) VALUES (?,?)',[login,senha]);
 
+    console.log('Dados inseridos com sucesso!');
 }
 
 criarTabela();
